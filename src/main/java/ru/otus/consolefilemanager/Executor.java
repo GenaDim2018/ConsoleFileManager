cd9a55f5be13fd3804b6d99f59ec9b70166e7d35
GenaDim2018 <genadim2018@gmail.com>
2023-11-09 16:59:44 +0300

    added mv, cp, finfo, help

--- src/main/java/ru/otus/consolefilemanager/Executor.java
+++ src/main/java/ru/otus/consolefilemanager/Executor.java
@@ -1,8 +1,7 @@
 package ru.otus.consolefilemanager;
 
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.Date;
 
 import static ru.otus.consolefilemanager.Manager.readLine;
@@ -65,11 +64,8 @@ public class Executor {
 
     public void mkdir(String name) {
         File newDir = new File(filePath.getPath() + "/" + name);
-        try {
-            newDir.mkdir();
-        } catch (UnknownError e) {
-            System.out.println("Некорректное имя директории");
-        }
+        if (newDir.mkdir()) System.out.println("Директория " + name + " успешно создана");
+        else System.out.println("Некорректное имя директории");
     }
 
     public void rm(String name) {
@@ -84,11 +80,12 @@ public class Executor {
                     System.out.println("Директория не пуста, удалить? (Да/Нет)");
                     if (readLine().equals("Да")) {
                         if (deleteDirectory(rmFile)) System.out.println("Директория успешно удалена");
-                        else System.out.println("Не удалось полностью удалить директорию, возможно сейчас открыт файл из нее");
+                        else
+                            System.out.println("Не удалось полностью удалить директорию, возможно сейчас открыт файл из нее");
                     }
                 }
             }
-        }
+        } else System.out.println("Неверное имя файла/директории");
     }
 
     public boolean deleteDirectory(File directory) {
@@ -99,6 +96,50 @@ public class Executor {
             }
         }
         return directory.delete();
+    }
+
+    public void mv(String name, String destination) {
+        File mvFile = new File(filePath.getPath() + "/" + name);
+        File dest = new File(destination);
+        File mv = new File(destination + "/" + name);
+        if (checkDirectory(mvFile) && checkDirectory(dest)) {
+            mvFile.renameTo(mv);
+            System.out.println("Файл/директория успешно перемещена");
+        } else System.out.println("Некорректное имя файла/директории или путь");
+    }
+
+    public void cp(String name, String destination) {
+        File cpFile = new File(filePath.getPath() + "/" + name);
+        File dest = new File(destination);
+        if (checkDirectory(cpFile) && checkDirectory(dest) && cpFile.isFile()) {
+            File newFile = new File(dest.getPath() + "/" + name);
+            try {
+                if (!newFile.createNewFile()) System.out.println("Файл с таким именем уже существует");
+                return;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try (InputStream in = new FileInputStream(cpFile); OutputStream out = new FileOutputStream(newFile)) {
+                newFile.createNewFile();
+                byte[] buf = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = in.read(buf)) > 0) {
+                    out.write(buf, 0, bytesRead);
+                }
+                System.out.println("Скопировано успешно");
+            } catch (IOException e) {
+                System.out.println("Не удалось скопировать");
+                e.printStackTrace();
+            }
+        } else System.out.println("Некорректное имя файла/директории или путь");
+    }
+
+    public void finfo(String name) {
+        File file = new File(filePath.getPath() + "/" + name);
+        if (checkDirectory(file) && file.isFile()) {
+            Date date = new Date(file.lastModified());
+            System.out.println(file.getName() + " " + file.length() + " Б, последнее изменение " + date);
+        } else System.out.println("Неверное имя файла");
     }
 
     public boolean checkDirectory(File dir) {
--- src/main/java/ru/otus/consolefilemanager/Parser.java
+++ src/main/java/ru/otus/consolefilemanager/Parser.java
@@ -17,7 +17,7 @@ public class Parser {
             String[] commArgs = new String[2];
             if (command.contains(" ")) {
                 commArgs[0] = command.substring(0, command.indexOf(" "));
-                commArgs[1] = command.substring(command.indexOf(" ")+1);
+                commArgs[1] = command.substring(command.indexOf(" ") + 1);
             } else commArgs[0] = command;
             switch (commArgs[0]) {
                 case ("ls") -> {
@@ -40,6 +40,53 @@ public class Parser {
                 case ("rm") -> {
                     if (commArgs[1] == null) System.out.println(unknownCommand);
                     else executor.rm(commArgs[1]);
+                }
+                case ("mv") -> {
+                    if (commArgs[1] == null) System.out.println(unknownCommand);
+                    else {
+                        if (commArgs[1].contains(" ") && commArgs[1].contains("\\")) {
+                            int regexIndex = commArgs[1].lastIndexOf(" ", commArgs[1].lastIndexOf("\\"));
+                            String destination = commArgs[1].substring(regexIndex + 1);
+                            String fileName = commArgs[1].substring(0, regexIndex);
+                            executor.mv(fileName, destination);
+                        } else System.out.println(unknownCommand);
+                    }
+
+                }
+                case ("cp") -> {
+                    if (commArgs[1] == null) System.out.println(unknownCommand);
+                    else {
+                        if (commArgs[1].contains(" ") && commArgs[1].contains("\\")) {
+                            int regexIndex = commArgs[1].lastIndexOf(" ", commArgs[1].lastIndexOf("\\"));
+                            String destination = commArgs[1].substring(regexIndex + 1);
+                            String fileName = commArgs[1].substring(0, regexIndex);
+                            executor.cp(fileName, destination);
+                        } else System.out.println(unknownCommand);
+                    }
+                }
+                case ("finfo") -> {
+                    if (commArgs[1] == null) System.out.println(unknownCommand);
+                    else {
+                        executor.finfo(commArgs[1]);
+                    }
+                }
+                //C:\Users\genad\Desktop\java
+                case ("help") -> {
+                    if (commArgs[1] == null){
+                        System.out.println("ls – распечатать список файлов текущего каталога");
+                        System.out.println("ls -l – распечатать список файлов текущего каталога c информаций о файлах");
+                        System.out.println("cd [имя директории] – переход в указанную поддиректорию");
+                        System.out.println("cd .. – переход в родительский каталог");
+                        System.out.println("mkdir [имя директории] – создание новой директории с указанным именем");
+                        System.out.println("rm [имя файла/директории] – удаление указанного файла или директории");
+                        System.out.println("mv [имя файла/директории] [путь/новое имя] – переименовать/перенести файл или директорию");
+                        System.out.println("cp [имя файла] [путь копирования] – скопировать файл");
+                        System.out.println("finfo [имя файла] – получить подробную информацию о файле");
+                        System.out.println("help – вывод в консоль всех поддерживаемых команд");
+                        System.out.println("find [filename] – найти файл с указанным именем в текущем каталоге или любом его подкаталоге");
+                        System.out.println("exit – завершить работу");
+                    }
+                    else System.out.println(unknownCommand);
                 }
                 case ("exit") -> {
                     if (commArgs[1] == null) System.exit(1);
